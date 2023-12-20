47365da7fc914d8e8c3b23011c6f962df574058d
ratulhasan001 <ratulhasan2108@gmail.com>
2023-12-20 10:56:40 +0600

    backtracking updated

---
+++ Backtracking/backtrack.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool vis[20];
+
+void back(int n, vector < int > v) {
+    for (int x: v)
+        cout << x << " ";
+    cout << '\n';
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            vis[i] = true;
+            v.push_back(i);
+            back(n, v); // waiting
+            vis[i] = false;
+            v.pop_back();
+        }
+    }
+}
+int main() {
+    int n;
+    cin >> n;
+    memset(vis, false, sizeof(vis));
+    back(n, {});
+    return 0;
+}
