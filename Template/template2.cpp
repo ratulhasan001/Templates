fb97cfecafe4f381acfc5c510ca838912358066e
ratulhasan001 <ratulhasan2108@gmail.com>
2024-01-11 10:55:08 +0600

    updated

--- Template/template2.cpp
+++ Template/template2.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
+#include<ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
 using namespace std;
 using ll = long long;
+using pii = pair < int, int >;
+template <typename T> using ordered_set = tree<T, null_type, less<T>,
+rb_tree_tag,  tree_order_statistics_node_update>;
 
 #ifdef ONLINE_JUDGE
-#define debug(...) 42
-#define debug_array(x,y) 42
+#define debug(...) 31
+#define debug_array(x,y) 31
 #else
 #include "deBUG.h"
 #endif
@@ -20,7 +27,7 @@ int32_t main() {
 
     int T; 
     cin >> T;
-    for(int testCase=1; testCase <= T; testCase++) {
+    for(int testCase = 1; testCase <= T; testCase++) {
     
         attack();
         // cout << "Case " << testCase << ": " << ;
